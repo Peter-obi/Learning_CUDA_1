f95072cd77e29969ffe97d81af354e1a9ea25727
Peter Obi <52758657+Peter-obi@users.noreply.github.com>
2025-11-17 20:04:56 -0500

    Create boundary_tiled_matmul.cu

---
+++ PMPP/chapter5/boundary_tiled_matmul.cu
@@ -0,0 +1,36 @@
+#define TILE_WIDTH 16
+
+__global__ void matrixMulkernel(float* M, float* N, float* P, int Width){
+
+    __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float Nds[TILE_WIDTH][TILE_WIDTH];
+
+    int bx = blockIdx.x; int tx = threadIdx.x;
+    int by = blockIdx.y; int ty = threadIdx.y;
+
+    //Identify the row and column of the P element to work on
+    int Row = by * TILE_WIDTH + ty;
+    int Col = bx * TILE_WIDTH + tx;
+
+    //Loop over M and N tiles to compute P element
+    float Pvalue = 0;
+    for(int ph = 0; ph < ceil(Width/ (float) TILE_WIDTH); ++ph){
+
+        //Collaborative loading of M and N into shared memory with boundary check
+        if ((Row < Width) && (ph*TILE_WIDTH+tx) < Width)
+            Mds[ty][tx] = M[Row*Width + ph*TILE_WIDTH + tx];
+        else Mds[ty][tx] = 0.0f;
+        if((ph*TILE_WIDTH + ty) < Width && (Col < Width))
+            Nds[ty][tx] = N[(ph*TILE_WIDTH + ty) * Width + Col];
+        else  Nds[ty][tx] = 0.0f;
+        __syncthreads();
+
+        for(int k = 0; k < TILE_WIDTH; ++k){
+            Pvalue += Mds[ty][k] * Nds[k][tx];
+        }
+        __syncthreads();
+    }
+    if(Row < Width && Col < Width)
+        P[Row*Width + Col] = Pvalue;
+
+}
